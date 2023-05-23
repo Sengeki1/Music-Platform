4ab5a4c47b3b6b4f09670dcefc3ddce85689662e
Sengeki1 <gamersengeki@gmail.com>
2023-05-23 21:39:17 -0100

    changed the pictures directory

--- searchEngine.js
+++ searchEngine.js
@@ -1,68 +1,68 @@
 const songs = [
     {
         id: '01',
-        image: 'img/Cesaria Evora.jpg',
-        title: 'Cesária Évora',
+        image: 'img/01.jpg',
+        name: 'Cesária Évora',
         nameOfSong: 'Sodade'
     },
     {
         id: '02',
-        image: 'img/Ildo Lobo.jpg',
-        title: 'Ildo Lobo',
+        image: 'img/02.jpg',
+        name: 'Ildo Lobo',
         nameOfSong: 'Nós Morna'
     },
     {
         id: '03',
-        image: 'img/Tito Paris.jpg',
-        title: 'Tito Paris',
+        image: 'img/03.jpg',
+        name: 'Tito Paris',
         nameOfSong: 'Fica ma mi'
     },
     {
         id: '04',
-        image: 'img/Bau.jpg',
-        title: 'Bau',
+        image: 'img/04.jpg',
+        name: 'Bau',
         nameOfSong: 'Raquel'
     },
     {
         id: '05',
-        image: 'img/Bana.jpg',
-        title: 'Bana',
+        image: 'img/05.jpg',
+        name: 'Bana',
         nameOfSong: 'Nha Fidjo Matcho'
     },
     {
         id: '06',
-        image: 'img/Ildo Lobo.jpg',
-        title: 'Ildo Lobo',
+        image: 'img/06.jpg',
+        name: 'Ildo Lobo',
         nameOfSong: 'Amor e Mar'
     },
     {
         id: '07',
-        image: 'img/Cesaria Evora.jpg',
-        title: 'Cesária Évora',
+        image: 'img/07.jpg',
+        name: 'Cesária Évora',
         nameOfSong: 'Mar Azul'
     },
     {
         id: '08',
-        image: 'img/Cesaria Evora.jpg',
-        title: 'Cesária Évora',
+        image: 'img/08.jpg',
+        name: 'Cesária Évora',
         nameOfSong: 'Angola'
     },
     {
         id: '09',
-        image: 'img/Cesaria Evora.jpg',
-        title: 'Cesária Évora',
+        image: 'img/09.jpg',
+        name: 'Cesária Évora',
         nameOfSong: 'Miss Perfumado'
     },
     {
         id: '10',
-        image: 'img/Ildo Lobo.jpg',
-        title: 'Ildo Lobo',
+        image: 'img/10.jpg',
+        name: 'Ildo Lobo',
         nameOfSong: 'Incondicional'
     },
     {
         id: '11',
-        image: 'img/Ildo Lobo.jpg',
-        title: 'Ildo Lobo',
+        image: 'img/11.jpg',
+        name: 'Ildo Lobo',
         nameOfSong: 'Pamodi'
     }
 ]
@@ -73,7 +73,7 @@ document.getElementById('input_txt').addEventListener('keyup', (e) => {
     const searchData = e.target.value.toLowerCase();
     const filterData = categories.filter((item) => {
         return(
-            item.nameOfSong.toLocaleLowerCase().includes(searchData) || item.title.toLocaleLowerCase().includes(searchData)
+            item.nameOfSong.toLocaleLowerCase().includes(searchData) || item.name.toLocaleLowerCase().includes(searchData)
         )
     })
     displayItem(filterData)
@@ -81,14 +81,14 @@ document.getElementById('input_txt').addEventListener('keyup', (e) => {
 
 const displayItem = (items) => {
     document.getElementById('root').innerHTML = items.map((item) => {
-        var {id, image, title, nameOfSong} = item;
+        var {id, image, name, nameOfSong} = item;
         return(
             `<li class="songItem">
                 <i class="bi playList bi-play-fill" id="${id}"></i>
                 <span>${id}</span>
-                <img src="${image}">
+                <img src="${image}" alt="">
                 <h5>${nameOfSong}
-                    <div class="subtitle">${title}</div>
+                    <div class="subtitle">${name}</div>
                 </h5>
             </li>`
         )
