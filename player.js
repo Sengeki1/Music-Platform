c547d6bac7e85b94d80a998cd7530ac42b152679
Sengeki1 <gamersengeki@gmail.com>
2023-05-23 21:37:41 -0100

    function and responsive player

--- player.js
+++ player.js
@@ -1,73 +1,73 @@
 const music = new Audio('audio/01.mp3');
 
-/*const songsPlayer = [
+const songsPlayer = [
     {
         id: '01',
         songName: `Sodade <br>
         <div class="subtitle">Cesária Évora</div>`,
-        poster: "img/Cesaria Evora.jpg"
+        poster: "img/01.jpg"
     },
     {
         id: '02',
         songName: `Nós Morna <br>
         <div class="subtitle">Ildo Lobo</div>`,
-        poster: "img/Ildo Lobo.jpg"
+        poster: "img/02.jpg"
     },
     {
         id: '03',
         songName: `Fica ma mi <br>
         <div class="subtitle">Tito Paris</div>`,
-        poster: "img/Tito Paris.jpg"
+        poster: "img/03.jpg"
     },
     {
         id: '04',
         songName: `Raquel <br>
         <div class="subtitle">Bau</div>`,
-        poster: "img/Bau.jpg"
+        poster: "img/04.jpg"
     },
     {
         id: '05',
         songName: `Nha Fidjo Matcho <br>
         <div class="subtitle">Bana</div>`,
-        poster: "img/Bana.jpg"
+        poster: "img/05.jpg"
     },
     {
         id: '06',
         songName: `Amor e Mar <br>
         <div class="subtitle">Ildo Lobo</div>`,
-        poster: "img/Ildo Lobo.jpg"
+        poster: "img/06.jpg"
     },
     {
         id: '07',
         songName: `Mar Azul <br>
         <div class="subtitle">Cesária Évora</div>`,
-        poster: "img/Cesaria Evora.jpg"
+        poster: "img/07.jpg"
     },
     {
         id: '08',
         songName: `Angola <br>
         <div class="subtitle">Cesária Évora</div>`,
-        poster: "img/Cesaria Evora.jpg"
+        poster: "img/08.jpg"
     },
     {
         id: '09',
         songName: `Miss Perfumado <br>
         <div class="subtitle">Cesária Évora</div>`,
-        poster: "img/Cesaria Evora.jpg"
+        poster: "img/09.jpg"
     },
     {
         id: '10',
         songName: `Incondicional <br>
         <div class="subtitle">Ildo Lobo</div>`,
-        poster: "img/Ildo Lobo.jpg"
+        poster: "img/10.jpg"
     },
     {
         id: '11',
         songName: `Pamodi <br>
         <div class="subtitle">Ildo Lobo</div>`,
-        poster: "img/Ildo Lobo.jpg"
+        poster: "img/11.jpg"
     }
-]*/
+]
 
 /*Array.from(document.getElementsByClassName('songItem')).forEach((e, i) => {
     e.getElementsByTagName('img')[0].src = songsPlayer[i].poster;
@@ -78,7 +78,7 @@ let masterPlay = document.getElementById('masterPlay');
 let wave = document.getElementById('wave');
 
 masterPlay.addEventListener('click', () => {
-    if (music.paused || music.currentTime <= 0){
+    if (music.paused || music.currentTime < 0){
         music.play();
         wave.classList.add('active1');
         masterPlay.classList.remove('bi-play-fill')
@@ -92,21 +92,36 @@ masterPlay.addEventListener('click', () => {
 });
 
 let index = 0;
-
+let poster_master_play = document.getElementById('poster_master_player');
+let title = document.getElementById('title');
+Array.from(document.getElementsByClassName('playList')).forEach((e) => {
+    e.addEventListener('click', (al) => {
+        index = al.target.id;
+    })
+});
 Array.from(document.getElementsByClassName('playList')).forEach((e) => {
     e.addEventListener('click', (el) => {
         index = el.target.id;
         music.src = `audio/${index}.mp3`;
-        if (music.paused || music.currentTime <= 0){
+        poster_master_play.src = `img/${index}.jpg`;
+        masterPlay.classList.remove('bi-play-fill');
+        masterPlay.classList.add('bi-pause-fill');
+        if (music.paused || music.currentTime < 0){
             music.play();
             wave.classList.add('active1');
-            masterPlay.classList.remove('bi-play-fill');
-            masterPlay.classList.add('bi-pause-fill');
         } else {
             music.pause();
             wave.classList.remove('active1');
-            masterPlay.classList.add('bi-play-fill');
-            masterPlay.classList.remove('bi-pause-fill');
         }
+
+        let songTitles = songsPlayer.filter((els) => {
+            return els.id == index;
+        });
+
+        songTitles.forEach(elss => {
+            let {songName} = elss;
+            title.innerHTML = songName;
+        })
+
     })
-})
+});
