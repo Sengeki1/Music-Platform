473a4bbf3b962f2af9491ecc5e3e9e9d2ddc3cd6
Sengeki1 <gamersengeki@gmail.com>
2023-05-23 12:57:04 -0100

    fixed number tracks

--- searchEngine.js
+++ searchEngine.js
@@ -1,70 +1,70 @@
 const songs = [
     {
-        id: 1,
+        id: '01',
         image: 'img/Cesaria Evora.jpg',
         title: 'Cesária Évora',
         nameOfSong: 'Sodade'
     },
     {
-        id: 2,
+        id: '02',
         image: 'img/Ildo Lobo.jpg',
         title: 'Ildo Lobo',
         nameOfSong: 'Nós Morna'
     },
     {
-        id: 3,
+        id: '03',
         image: 'img/Tito Paris.jpg',
         title: 'Tito Paris',
         nameOfSong: 'Fica ma mi'
     },
     {
-        id: 4,
+        id: '04',
         image: 'img/Bau.jpg',
         title: 'Bau',
         nameOfSong: 'Raquel'
     },
     {
-        id: 5,
+        id: '05',
         image: 'img/Bana.jpg',
         title: 'Bana',
         nameOfSong: 'Nha Fidjo Matcho'
     },
     {
-        id: 6,
+        id: '06',
         image: 'img/Ildo Lobo.jpg',
         title: 'Ildo Lobo',
         nameOfSong: 'Amor e Mar'
     },
     {
-        id: 7,
+        id: '07',
         image: 'img/Cesaria Evora.jpg',
         title: 'Cesária Évora',
         nameOfSong: 'Mar Azul'
     },
     {
-        id: 8,
+        id: '08',
         image: 'img/Cesaria Evora.jpg',
         title: 'Cesária Évora',
         nameOfSong: 'Angola'
     },
     {
-        id: 9,
+        id: '09',
         image: 'img/Cesaria Evora.jpg',
         title: 'Cesária Évora',
         nameOfSong: 'Miss Perfumado'
     },
     {
-        id: 10,
+        id: '10',
         image: 'img/Ildo Lobo.jpg',
         title: 'Ildo Lobo',
         nameOfSong: 'Incondicional'
     },
     {
-        id: 11,
+        id: '11',
         image: 'img/Ildo Lobo.jpg',
         title: 'Ildo Lobo',
         nameOfSong: 'Pamodi'
-    },
+    }
 ]
 
 const categories = [...new Set(songs.map((item) => {return item}))]
@@ -81,9 +81,10 @@ document.getElementById('input_txt').addEventListener('keyup', (e) => {
 
 const displayItem = (items) => {
     document.getElementById('root').innerHTML = items.map((item) => {
-        var {image, title, nameOfSong} = item;
+        var {id, image, title, nameOfSong} = item;
         return(
             `<li class="songItem">
+                <span>${id}</span>
                 <img src="${image}">
                 <h5>${nameOfSong}
                     <div class="subtitle">${title}</div>
