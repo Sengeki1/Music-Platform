0d3270aa301e635e2fd4afa12bd83d31454ac667
Sengeki1 <gamersengeki@gmail.com>
2023-05-23 12:57:56 -0100

    some alterations

--- player.js
+++ player.js
@@ -1,2 +1,71 @@
 const music = new Audio('audio/Cesária Evora/Sodade.mp3');
-music.play();
+//music.play();
+
+const songsPlayer = [
+    {
+        id: '01',
+        songName: `Sodade <br>
+        <div class="subtitle">Cesária Évora</div>`,
+        poster: "img/Cesaria Evora.jpg"
+    },
+    {
+        id: '02',
+        songName: `Nós Morna <br>
+        <div class="subtitle">Ildo Lobo</div>`,
+        poster: "img/Ildo Lobo.jpg"
+    },
+    {
+        id: '03',
+        songName: `Fica ma mi <br>
+        <div class="subtitle">Tito Paris</div>`,
+        poster: "img/Tito Paris.jpg"
+    },
+    {
+        id: '04',
+        songName: `Raquel <br>
+        <div class="subtitle">Bau</div>`,
+        poster: "img/Bau.jpg"
+    },
+    {
+        id: '05',
+        songName: `Nha Fidjo Matcho <br>
+        <div class="subtitle">Bana</div>`,
+        poster: "img/Bana.jpg"
+    },
+    {
+        id: '06',
+        songName: `Amor e Mar <br>
+        <div class="subtitle">Ildo Lobo</div>`,
+        poster: "img/Ildo Lobo.jpg"
+    },
+    {
+        id: '07',
+        image: 'img/Cesaria Evora.jpg',
+        title: 'Cesária Évora',
+        nameOfSong: 'Mar Azul'
+    },
+    {
+        id: '08',
+        songName: `Angola <br>
+        <div class="subtitle">Cesária Évora</div>`,
+        poster: "img/Cesaria Evora.jpg"
+    },
+    {
+        id: '09',
+        songName: `Miss Perfumado <br>
+        <div class="subtitle">Cesária Évora</div>`,
+        poster: "img/Cesaria Evora.jpg"
+    },
+    {
+        id: '10',
+        songName: `Incondicional <br>
+        <div class="subtitle">Ildo Lobo</div>`,
+        poster: "img/Ildo Lobo.jpg"
+    },
+    {
+        id: '11',
+        songName: `Pamodi <br>
+        <div class="subtitle">Ildo Lobo</div>`,
+        poster: "img/Ildo Lobo.jpg"
+    }
+]
