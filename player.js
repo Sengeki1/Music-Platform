e3cc1b60b721ce2773bfde28f204e5947f1fe644
Sengeki1 <gamersengeki@gmail.com>
2023-05-24 15:34:46 -0100

    functional bar & volume

--- player.js
+++ player.js
@@ -151,6 +151,9 @@ Array.from(document.getElementsByClassName('playList')).forEach((e) => {
 // Player timer
 let currentStart = document.getElementById('currentStart');
 let currentEnd = document.getElementById('currentEnd');
+let seek = document.getElementById('seek');
+let bar2 = document.getElementById('bar2');
+let dot = document.getElementsByClassName('dot')[0];
 
 music.addEventListener('timeupdate', () => {
     let music_curr = music.currentTime;
@@ -158,13 +161,54 @@ music.addEventListener('timeupdate', () => {
 
     let min1 = Math.floor(music_dur / 60);
     let sec1 = Math.floor(music_dur % 60);
+    if(sec1 < 10){
+        sec1 = `0${sec1}`;
+    }
     currentEnd.innerText = `${min1}:${sec1}`;
 
     let min2 = Math.floor(music_curr / 60);
     let sec2 = Math.floor(music_curr % 60);
-    currentStart.innerText = `${min2}:${sec2}`;
-
     if(sec2 < 10){
         sec2 = `0${sec2}`;
     }
+    currentStart.innerText = `${min2}:${sec2}`;
+
+    // bar
+    let progressBar = parseInt((music_curr / music_dur) * 100);
+    seek.value = progressBar;
+    let seekbar = seek.value;
+    bar2.style.width = `${seekbar}%`;
+    dot.style.left = `${seekbar}%`;
+})
+
+seek.addEventListener('change', () => {
+    music.currentTime = seek.value * music.duration / 100;
+})
+
+let vol_icon = document.getElementById('vol_icon');
+let vol = document.getElementById('vol');
+let vol_bar = document.getElementsByClassName('vol_bar')[0];
+let vol_dot = document.getElementById('vol_dot');
+
+vol.addEventListener('change', () => {
+    if(vol.value == 0){
+        vol_icon.classList.remove('bi-volume-up-fill');
+        vol_icon.classList.remove('bi-volume-down-fill');
+        vol_icon.classList.add('bi-volume-off-fill');
+    }
+    if(vol.value > 0){
+        vol_icon.classList.remove('bi-volume-up-fill');
+        vol_icon.classList.add('bi-volume-down-fill');
+        vol_icon.classList.remove('bi-volume-off-fill');
+    }
+    if(vol.value > 50){
+        vol_icon.classList.add('bi-volume-up-fill');
+        vol_icon.classList.remove('bi-volume-down-fill');
+        vol_icon.classList.remove('bi-volume-off-fill');
+    }
+
+    let vol_a = vol.value;
+    vol_bar.style.width = `${vol_a}%`;
+    vol_dot.style.left = `${vol_a}%`;
+    music.volume = vol_a / 100;
 })
