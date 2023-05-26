ddda465744b34350fd23930287472e49eee7c3cd
Sengeki1 <gamersengeki@gmail.com>
2023-05-26 10:21:30 -0100

    fixed the interactions events

--- login/login.js
+++ login/login.js
@@ -1,31 +1,50 @@
-
-    // Esta função realiza a validação do email e da senha de acordo com as expressões de padrão
-    function logar(){
-        let email = document.getElementById('email').value
-        let senha = document.getElementById('senha').value
-
-        let emailPadrao = /^[a-z0-9-]+(\.[a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-
-        let senhaPadrao = /^(?=.*[0-9])[a-z0-9!@#$%^&*]{6,12}$/;
-        
-        console.log('Email', emailPadrao.test(email));
-        console.log('Senha', senhaPadrao.test(senha));
-        
-        if(!email){ 
-            alert('Preencha o campo Email')
-        }else if(!senha){
-            alert('Prencha o campo Senha')
-        }else if( emailPadrao.test(email) && senhaPadrao.test(senha) !== true){
-            alert('Senha invalida')
-        }else if(emailPadrao.test(email) !== true && senhaPadrao.test(senha)){
-            alert('Formato de e-mail invalido')
-        }else if(emailPadrao.test(email) && senhaPadrao.test(senha)){
-            alert('Login com Sucesso')
-            //window.location = 'registrar.html'
-            window.location.href='listaTarefa.html'
-        }else{
-            //console.log('Email', emailPadrao.test(email)); // mostra no console true or false
-            alert('Dados invalidos', emailPadrao.test(email), senhaPadrao.test(senha))
-        }   
+function myfun() {
+  
+    const emailAddress = document.getElementById('email').value;
+    const passWord = document.getElementById('pw').value; 
+  
+    const Email = localStorage.getItem("Email");
+    const Password = localStorage.getItem("Password");
+  
+    if (emailAddress == "" || passWord == "")
+    {
+      swal("Opps...!", "Input field must be fill!", "error");
+    } else if (emailAddress == Email && passWord == Password)
+    {
+      setTimeout(function () {
+        window.location.href = "../index.html"
+      }, 1000);
+      swal("Good job!", "Login Successful!", "success");
+    } else 
+    {
+      swal("Opps...!", "Something is Wrong!\n Check Password or Email!", "error");
     }
+  
+}
+  
+function checker() {
+  
+    let emailId = document.getElementById('email');
+    let icon = document.getElementById('icon2');
+    let mailFormat =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  
+    icon.style.display = "inline-block";
+    
+    if (emailId.value.match(mailFormat)) {
+      
+      icon.innerHTML = '<i class="fas fa-circle-check"></i>';
+      icon.style.color = '#008000';
+  
+    } else if (emailId.value == "") {
+      
+      icon.style.display = 'none';
+  
+    } else {
+  
+      icon.innerHTML = '<i class="fas fa-exclamation-circle"></i>';
+      icon.style.color = '#ff2851';
+  
+    }
+  
+}
    
