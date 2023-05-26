bfdb59b71736f60c5cb633f791f6a83335110580
Sengeki1 <gamersengeki@gmail.com>
2023-05-26 10:18:29 -0100

    fixed the interactions events

--- register/registrar.js
+++ register/registrar.js
@@ -1,68 +1,128 @@
+function validation() {
 
-
-
-/*
-let nome = document.querySelector('#nome')
-let sobrenome = document.querySelector('#sobrenome')
-let email = document.querySelector('#email')
-let senha = document.querySelector('#senha')
-let confirm_senha = document.querySelector('#confirm_senha')
-*/
-let nome_padrao = /^[a-z0-9]+(\.[a-z0-9])$/
-let sobrenome_padrao = /^[a-z0-9]+[a-z0-9]$/
-let email_padrao = /^[a-z0-9]+(\.[a-z0-9]+)*@[a-z0-9]+(\.[a-z0-9]+)*(\.[a-z]{2,4})$/
-let senha_padrao = /^(?=.*[0-9])[a-z0-9!@#$%^&*]{6,12}$/
-
-
-
-function registrar(){
-
-    let nome = document.getElementById('nome').value
-    let sobrenome = document.getElementById('sobrenome').value
-    let email = document.getElementById('email').value
-    let senha = document.getElementById('senha').value
-    let confirm_senha = document.getElementById('confirm_senha').value
-
-    let nome_padrao = /^[A-Za-z]+[a-z]$/
-    let sobrenome_padrao = /^[A-Za-z]+[a-z]$/
-    let email_padrao = /^[a-z0-9]+(\.[a-z0-9]+)*@[a-z0-9]+(\.[a-z0-9]+)*(\.[a-z]{2,4})$/
-    let senha_padrao = /^(?=.*[0-9])[a-z0-9!@#$%^&*]{6,12}$/
-
-    if(!nome){
-        alert('Preencha o campo "Nome"')
-    } else if(!sobrenome){
-        alert('Preencha o campo "Sobrenome"')
-    } else if(!email){
-        alert('Preencha o campo "Email"') 
-    } else if(!senha){
-        alert('Preencha o campo "Senha"')
-    } else if(!confirm_senha){
-        alert('Preencha o campo "Confirmar Senha"')
-    } else if(nome_padrao.test(nome) !== true){
-        alert('Nome inválido!')
-    } else if(sobrenome_padrao.test(sobrenome) !== true){
-        alert('Sobrenome inválido')
-    } else if(email_padrao.test(email) !== true){
-        alert('Email inválido')
-    } else if (senha_padrao.test(senha) !== true){
-        alert('Senha inválida')
-    } else if(confirm_senha !== senha){
-        alert('A confirmação da senha esta incorreta')
-    } else if (nome_padrao.test(nome) && sobrenome_padrao.test(sobrenome) && email_padrao.test(email) && senha_padrao.test(senha) && confirm_senha === senha){
-        window.location.href='login.html'
-        alert('Registro com sucesso!')
-    } else {
-        alert('Dados invalidos', nome_padrao.test(nome), sobrenome_padrao.test(sobrenome), email_padrao.test(email), senha_padrao.test(senha), confirm_senha.test(confirm_senha))
-    }
-
-    console.log('Nome', nome_padrao.test(nome))
-    console.log('Sobrenome', sobrenome_padrao.test(sobrenome))
-    console.log('Email', email_padrao.test(email));
-    console.log('Senha', senha_padrao.test(senha));
+    let emailId = document.getElementById('email');
+    let icon = document.getElementById('icon1');
+    let mailFormat =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     
-}
-
-
-
-
-
+    icon.style.display = "inline-block";
+      
+    if (emailId.value.match(mailFormat)) {
+        
+      icon.innerHTML = '<i class="fas fa-circle-check"></i>';
+      icon.style.color = '#008000';
+      return true;
+    
+    } else if (emailId.value == "") {
+        
+      icon.style.display = 'none';
+      return false;
+    
+    } else {
+    
+      icon.innerHTML = '<i class="fas fa-exclamation-circle"></i>';
+      icon.style.color = '#ff2851';
+      return false;
+    
+    }
+    
+  }
+  
+  function see () {
+    
+    let input = document.getElementById('pw');
+    let input2 = document.getElementById('confirm-pw');
+  
+    if (input.type === "password" && input2.type === "password") {
+      
+      input.type = "text";
+      input2.type = "text";
+  
+    } else {
+  
+      input.type = "password";
+      input2.type = "password";
+  
+    }
+  }
+  
+  function confirm() {
+  
+    let password = document.getElementById('pw');
+    let confirmPass = document.getElementById('confirm-pw');
+    let icon = document.getElementById('icon2');
+  
+    if (password.value == confirmPass.value) {
+  
+      icon.innerHTML = '<i class="fas fa-circle-check"></i>';
+      icon.style.color = "#008000";
+  
+    } else {
+  
+      icon.innerHTML = '<i class="fas fa-exclamation-circle"></i>';
+      icon.style.color = "#ff2851";
+      
+    }
+  }
+  
+  
+  function check () {
+  
+    let pass = document.getElementById('pw');
+    let message = document.getElementById('error');
+    var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}/;
+  
+    if (pass.value.match(pattern)) {
+      message.innerText = "Your password is valid";
+      message.style.color = "green";
+    } else {
+      message.innerText = "The password is to weak";
+      message.style.color = "red";
+    }
+  
+  }
+  
+  function myfun() {
+    
+    const x1 = document.getElementById('username').value;
+    const x2 = document.getElementById('email').value;
+    const x3 = document.getElementById('pw').value;
+    const x4 = document.getElementById('confirm-pw').value;
+    const x5 = document.getElementById('m');
+    const x6 = document.getElementById('f');
+    const x7 = document.getElementById('error');
+  
+    localStorage.setItem("Username", x1);
+    localStorage.setItem("Email", x2);
+    localStorage.setItem("Password", x3);
+    localStorage.setItem("Confirm-pw", x4);
+    localStorage.setItem("male", x5);
+    localStorage.setItem("female", x6)
+  
+    if (x1 == "" || x2 == "" || x3 == "" || x4 == "") {
+      swal("Opps...!", "Input field must be fill!", "error");
+    }
+    else if (x5.checked != true && x6.checked != true)
+    {  
+      swal("Opps...!", "Please check your Gender!", "error");
+    }
+    else if (x7.innerText != "Your password is valid")
+    {
+      swal("Opps...!", "Please check your Password!", "error");
+    }
+    else if (validation() == false)
+    {
+      swal("Opps...!", "Please check your Email!", "error");
+    } 
+    else if (x3 != x4) 
+    {
+      swal("Opps...!", "Please check your Password!", "error");
+    }
+    else 
+    {
+      setTimeout(function () {
+        window.location.href = "../index.html"
+      }, 1000);
+      swal("Good job!", "Registration Successful!", "success");
+    }
+  }
+  
