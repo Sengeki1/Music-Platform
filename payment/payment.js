1a0a01a9d9e9776bea0d02494e511ccdd7aae841
Sengeki1 <gamersengeki@gmail.com>
2023-05-29 23:13:45 -0100

    functional & responsive form

--- payment/payment.js
+++ payment/payment.js
@@ -1,35 +1,29 @@
+function cardSpace() {
+  const carddigit = document.getElementById('card').value;
+    if(carddigit.length == 4 || carddigit.length == 9 || carddigit.length == 14){
+      document.getElementById('card').value = carddigit+" ";
+      document.getElementById('card').max = 1;
+    }
+}
+function addSlashes() {
+  const expiredate = document.getElementById('date').value;
+    if(expiredate.length == 2){
+      document.getElementById('date').value = expiredate+"/";
+      document.getElementById('date').max = 1;
+    }
+}
+
 function myfun() {
-    
-    const x1 = document.getElementById('name').value;
-    const x2 = document.getElementById('card').value;
-    const x3 = document.getElementById('date').value;
-    const x4 = document.getElementById('cvc').value;
-  
-    localStorage.setItem("name", x1);
-    localStorage.setItem("card", x2);
-    localStorage.setItem("date", x3);
-    localStorage.setItem("cvc", x4);
-  
-    if (x1 == "" || x2 == "" || x3 == "" || x4 == "") {
-      swal("Opps...!", "Input field must be fill!", "error");
-    }
-    else if (x2.innerText != true)
-    {
-      swal("Opps...!", "Please check your Card Number!", "error");
-    }
-    else if (x3 != true) 
-    {
-      swal("Opps...!", "Please check the expire date!", "error");
-    }
-    else if (x4 != true)
-    {
-      swal("Opps...!", "Please check your CVC!", "error");
-    }
-    else 
-    {
+  const card = document.getElementById('card').value;
+  const date = document.getElementById('date').value;
+  const name = document.getElementById('name').value;
+  const cvc = document.getElementById('cvc').value;
+  if(card == "" || date == "" || name == "" || cvc == ""){
+    swal("Opps...!", "Input field must be fill!", "error");
+  } else{
       setTimeout(function () {
         window.location.href = "../index.html"
       }, 1000);
       swal("Good job!", "Payment Successful!", "success");
-    }
   }
+}
